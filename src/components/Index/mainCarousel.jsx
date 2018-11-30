fdbbc6020bb5613eb7405b2c28bbc80c0cf3cbb2
dheeraj.v <dheeraj.v@msystechnologies.com>
2018-12-01 02:03:27 +0530

    url

--- src/components/Index/mainCarousel.jsx
+++ src/components/Index/mainCarousel.jsx
@@ -18,9 +18,9 @@ class MainCarousel extends Component {
             <div className="carousel-wrapper">
                 <Carousel autoplay effect="fade" ref={node => this.carousel = node}>
                     <div><img src="images/p2.jpg" className='carousel-image' /></div>
-                    <div className="bg2"><img src="../images/pic1.jpg" className='carousel-image' /></div>
-                    <div className="bg3"><img src="../images/p3.jpg" className='carousel-image' /></div>
-                    <div className="bg1"><img src="../images/p2.jpg" className='carousel-image' /> </div>
+                    <div className="bg2"><img src="images/pic1.jpg" className='carousel-image' /></div>
+                    <div className="bg3"><img src="images/p3.jpg" className='carousel-image' /></div>
+                    <div className="bg1"><img src="images/p2.jpg" className='carousel-image' /> </div>
                 </Carousel>
                 <Icon type="left" onClick={this.previous} />
                 <Icon type="right" onClick={this.next} />
